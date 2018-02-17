dc1cea1687b61f30dcf6a9077def0257c043a594
97968 <takahito_hashimoto@unirita.co.jp>
2018-02-17 15:02:07 +0900

    DataクラスとMyComparatorクラスを作成

--- java/Q9.java
+++ java/Q9.java
@@ -3,30 +3,60 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    void solve(){
         Scanner in = new Scanner(System.in);
 
         String[] sort = in.nextLine().split(" ");
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Data> list = new ArrayList<>();
 
         while (in.hasNextLine()) {
-            list.add(in.nextLine());
+            String s = in.nextLine();
+            list.add(new Data(s, s.split(" ")));
         }
 
-        list.sort(new Comparator<String>() {
-            public int compare(String s1, String s2) {
-                String[] ss1 = s1.split(" "), ss2 = s2.split(" ");
-                for (String st : sort) {
-                    int idx = st.charAt(0) - '0' - 1;
-                    if (ss1[idx].equals(ss2[idx])) {
-                        continue;
-                    }
-                    return ss1[idx].compareTo(ss2[idx]) * (st.charAt(1) == 'A' ? 1 : -1);
-                }
-                return 0;
-            }
-        });
+        list.sort(new MyComparator(sort));
 
         list.forEach(System.out::println);
+    }
+    
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+}
+
+class Data{
+    String line;
+    String[] cols;
+
+    public Data(String s, String[] ss){
+        this.line = s;
+        this.cols = ss;
+    }
+
+    @Override
+    public String toString(){
+        return line;
+    }
+}
+
+class MyComparator implements Comparator<Data> {
+
+    String[] sort;
+
+    public MyComparator(String[] ss){
+        super();
+        this.sort = ss;
+    }
+    @Override
+    public int compare(Data d1, Data d2){
+        for(String st : sort) {
+            int idx = st.charAt(0) - '0' -1;
+            if(d1.cols[idx].equals(d2.cols[idx])){
+                continue;
+            }
+            return d1.cols[idx].compareTo(d2.cols[idx]) * (st.charAt(1) == 'A' ? 1 : -1);
+        }
+        return 0;
     }
 }
