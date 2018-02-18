c647c9da6099fc670d7eca29803bf6c5dbeea533
97968 <takahito_hashimoto@unirita.co.jp>
2018-02-18 10:45:28 +0900

    値の入力を前後の切り分けの型をint型に

--- java/Q8.java
+++ java/Q8.java
@@ -4,15 +4,15 @@ public class Main{
     public static void main(String[] args)throws Exception{
         BufferedInputStream in = new BufferedInputStream(System.in);
         
-        byte b;
+        int i;
         StringBuilder num = new StringBuilder();
-        while((b = (byte)in.read()) != -1){
-            if(b == 0x0a){
+        while((i = in.read()) != -1){
+            if(i == 0x0a){
                 continue;
             }
             
-            byte upper4bit = (byte)((b>>4) & 0x0f); // upper 4 bit of b
-            byte lower4bit = (byte)(b & 0x0f);      // lower 4 bit of b
+            int upper4bit = ((i>>4) & 0x0f); // upper 4 bit of i
+            int lower4bit = (i & 0x0f);      // lower 4 bit of i
             if(lower4bit == 0x0d || lower4bit == 0x0c) {
                 num.append(String.format("%x", upper4bit));
                 if(num.charAt(0) == '0'){
@@ -24,7 +24,7 @@ public class Main{
                 System.out.println(num);
                 num = new StringBuilder();
             } else {
-                num.append(String.format("%02x", b));
+                num.append(String.format("%02x", i));
             }
         }
     }
